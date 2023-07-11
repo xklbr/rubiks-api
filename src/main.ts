86e45c4fa0a79433788d4e376a9c8bc8cc4e44b2
xklbr <3xkl1br@gmail.com>
2023-07-11 10:52:24 -0500

    edit port

--- src/main.ts
+++ src/main.ts
@@ -51,8 +51,8 @@ async function bootstrap() {
 
   app.enableShutdownHooks();
 
-  const port = process.env.PORT || 8000;
-  await app.listen(port);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   logger.log(`App running on port ${port}`);
 }
 bootstrap();
